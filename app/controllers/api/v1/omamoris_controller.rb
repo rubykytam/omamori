44ef198eeb293ea54f8c82d1097bf82d85da9fdb
Ruby Tam <pkamkam@gmail.com>
2024-03-25 15:47:17 +0900

    cloundinary linked

--- app/controllers/api/v1/omamoris_controller.rb
+++ app/controllers/api/v1/omamoris_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::OmamorisController < ApplicationController
   def index
     @omamoris = Omamori.all
-    render json: @omamoris.order(created_at: :desc)
+    render json: @omamoris.order(created_at: :desc).as_json(methods: [:photo_urls]), status: :ok
   end
 end
--- app/models/omamori.rb
+++ app/models/omamori.rb
@@ -1,4 +1,9 @@
+
 class Omamori < ApplicationRecord
 
   has_many_attached :photos
+
+  def photo_urls
+    photos.map { |photo| Cloudinary::Utils.cloudinary_url(photo.key) }
+  end
 end
