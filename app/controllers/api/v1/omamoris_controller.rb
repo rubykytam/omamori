8541120733a677fba8ec88a8d63442c994a84298
Ruby Tam <pkamkam@gmail.com>
2024-06-08 00:47:57 +0900

    serach by name

--- app/controllers/api/v1/omamoris_controller.rb
+++ app/controllers/api/v1/omamoris_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::OmamorisController < ApplicationController
   def index
+    if params[:name].present?
+      @omamoris = Omamori.where('title ILIKE ?', "%#{params[:name]}%")
+    else
     @omamoris = Omamori.all
+    end
     render json: @omamoris.order(created_at: :desc).as_json(methods: [:photo_urls]), status: :ok
   end
 end
