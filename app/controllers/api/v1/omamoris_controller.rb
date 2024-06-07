d0faa086300d8156331a0080b2560b36ac2bb59a
Ruby Tam <pkamkam@gmail.com>
2024-06-08 00:57:37 +0900

    add search into controller

--- app/controllers/api/v1/omamoris_controller.rb
+++ app/controllers/api/v1/omamoris_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::OmamorisController < ApplicationController
   def index
-    if params[:name].present?
-      @omamoris = Omamori.where('title ILIKE ?', "%#{params[:name]}%")
+    if params[:search].present?
+      @omamoris = Omamori.where("name ILIKE :search OR color ILIKE :search OR :search = ANY(tags)", search: "%#{params[:search]}%")
     else
-    @omamoris = Omamori.all
+     @omamoris = Omamori.all
     end
     render json: @omamoris.order(created_at: :desc).as_json(methods: [:photo_urls]), status: :ok
   end
