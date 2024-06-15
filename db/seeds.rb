c1e1659372f64d1999887f49009b8d1d1d49892d
Ruby Tam <pkamkam@gmail.com>
2024-06-16 02:24:42 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,17 @@
 #   end
 require 'open-uri'
 Omamori.destroy_all
-omamori1 = Omamori.new(name: "IT", jinja:"神田明神",tags:["紙"],color:"緑")
+omamori1 = Omamori.new(name: "IT", jinja:"神田明神",tags:["IT"],color:"緑")
 photo1 = URI.open("https://actindi.net/wp-content/uploads/2019/01/IMG_1119.jpg")
 omamori1.photos.attach(io: photo1, filename: "omamori.jpg", content_type: "image/jpg")
 omamori1.save
 
-omamori2 = Omamori.new(name: "縁結び", jinja:"今戶神社",tags:["巾著"],color:"黄色")
+omamori2 = Omamori.new(name: "縁結び", jinja:"今戶神社",tags:["縁結び"],color:"黄色")
 photo2 = URI.open("https://res.cloudinary.com/dtpr5icvx/image/upload/v1717770108/b65nrh9w3m5t6pwvm4df.jpg")
 omamori2.photos.attach(io: photo2, filename: "omamor2.jpg", content_type: "image/jpg")
 omamori2.save
 
-omamori3 = Omamori.new(name: "縁結び", jinja:"陽運寺",tags:["巾著","小さい"],color:"ピンク")
+omamori3 = Omamori.new(name: "縁結び", jinja:"陽運寺",tags:["縁結び"],color:"ピンク")
 photo3 = URI.open("https://res.cloudinary.com/dtpr5icvx/image/upload/v1717770108/kpoeybdoiip9eqiiorsd.jpg")
 omamori3.photos.attach(io: photo3, filename: "omamor3.jpg", content_type: "image/jpg")
 omamori3.save
